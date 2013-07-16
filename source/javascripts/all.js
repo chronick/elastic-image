a5eeeec7c892fcbceec02b6693e16f16727406b3
Nick Donohue <ndonohue@gmail.com>
2013-07-16 13:09:39 -0400

    adjustments

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,66 +1,111 @@
 //= require_tree .
+"use strict";
 
 document.addEventListener("DOMContentLoaded", function(){
   var logo = document.getElementById('logo');
-  var ultimateX = 0;
-  var ultimateY = 0
 
-  var x,y
+  var debug = false;
 
-  var smooth = function(val) {
+  var xCenter = 0;
+  var yCenter = 0
 
-    if (Math.abs(val) >= 0) {
-      return val;
+  var x,y,z
+  var xBuffer = []; 
+  var yBuffer = [];
+  var zBuffer = [];
+
+  var xOffset, yOffset, zOffset;
+
+  var width,height;
+  var left,top;
+
+  var zDrag = 5;
+  var xDrag = 8;
+  var yDrag = 8;
+
+  var smoothThreshold = 0.5;
+
+  var maxBuffer = 4;
+
+  var cssText;
+
+
+  var calculateCenter = function() {
+    xCenter = (document.width/2) - (logo.width/2);
+    yCenter = (document.width/2) - (logo.height/2);
+  }
+
+  var sum = function(array) {
+    return array.reduce(function(previousValue, currentValue){
+      return currentValue + previousValue;
+    });
+  };
+
+  var avg = function(array) {
+    return sum(array) / array.length;
+  }
+
+  var smooth = function(num) {
+    if (Math.abs(num) >= smoothThreshold) {
+      return num;
     }
-    else {
-      return 0;
+    else return 0;
+  }
+
+  var addToBuffer = function(array, current) {
+    if (array instanceof Array) {
+      if (array.length >= maxBuffer){
+        console.log(array.pop());
+      }
+      array.unshift(smooth(current));
     }
   };
 
-  window.addEventListener('devicemotion', function(event){
-    x = smooth(event.acceleration.x);
-    y = smooth(event.acceleration.y);
-    z = smooth(event.acceleration.z);
+  var currentAvg = function(array, current) {
+    addToBuffer(array, current);
+    return avg(array);
+  };
 
-    if (Math.abs(z) > 0){
-      logo.style.width = (150 + (z * 5)).toString() + "px";
-    }
-    else {
-      logo.style.width = "100px";
+  var motionHandler = function(event){
+    x = currentAvg(xBuffer, event.acceleration.x);
+    y = currentAvg(yBuffer, event.acceleration.y);
+    z = currentAvg(zBuffer, event.acceleration.z);
+
+    xOffset = Math.floor(x * xDrag);
+    yOffset = Math.floor(y * yDrag);
+    zOffset = z * zDrag;
+
+    left = (xCenter + xOffset);
+    top = (yCenter + yOffset);
+    width = (150 + zOffset);
+
+    cssText = "left: " + left.toString() + "px;"
+    + "top: " + top.toString() + "px;"
+    + "width: " + width.toString() + "px;";
+
+    // logo.style.left = left.toString() + "px";
+    // logo.style.top = top.toString() + "px";
+    // logo.style.width = (width).toString() + "px";
+
+    logo.style.cssText = cssText;
+
+    if(debug) {
+      document.getElementById('a-x').innerHTML = 'x' + x.toString();
+      document.getElementById('a-y').innerHTML = 'y' + y.toString();
+      document.getElementById('a-z').innerHTML = 'z' + z.toString();
     }
 
-    if ( Math.abs(x) > 0 ){
-      logo.style.left = (ultimateX + Math.floor(x * 5)).toString() + "px";
-    }
-    else {
-      logo.style.left = ultimateX;
-    }
-
-    if ( Math.abs(y) > 0 ){
-      logo.style.top = (ultimateY + Math.floor(y * 5)).toString() + "px";
-    }
-    else {
-      logo.style.top = ultimateY;
-    }
-
-
-    // document.getElementById('a-x').innerHTML = 'x' + smooth(event.acceleration.x).toString();
-    // document.getElementById('a-y').innerHTML = 'y' + smooth(event.acceleration.y).toString();
-    // document.getElementById('a-z').innerHTML = 'z' + smooth(event.acceleration.z).toString();
-
-  });
+    calculateCenter();
+  };
 
   var resizeHandler = function() {
-    logo.style.left = ((document.width / 2) - (logo.width/2)).toString() + "px";
-    logo.style.top = ((document.height /2) - (logo.height)).toString() + "px";
+    calculateCenter();
+    logo.style.left = xCenter.toString() + "px";
+    logo.style.top = yCenter.toString() + "px";
     logo.style.width = "100px";
-
-    ultimateX = parseInt(logo.style.left, 10);
-    ultimateY = parseInt(logo.style.top, 10);
-
-
   }
 
+  window.addEventListener('devicemotion', motionHandler);
   window.addEventListener('resize', resizeHandler);
   resizeHandler();
 });
